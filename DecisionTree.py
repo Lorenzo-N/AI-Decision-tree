761a98097e1661d71e7f07b7d3a0796a3daa7f74
alessionuti <alessionuti.19@gmail.com>
2018-12-24 17:46:05 +0100

    Tree print and dataset load

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,15 +1,73 @@
+import itertools
+
+
 class Node:
-    def __init__(self, key):
-        self.key = key
+    def __init__(self, value):
+        self.value = value
         self.children = []
 
+    def __str__(self):
+        return "" + str(self.value);
+
     def get(self):
-        return self.key
+        return self.value
 
     def add_child(self, child):
         self.children.append(child)
 
 
-class DecisionTree:
-    def __init__(self):
-        self.root = None
+class Options(object):
+    FORK = u'\u251c'
+    LAST = u'\u2514'
+    VERTICAL = u'\u2502'
+    HORIZONTAL = u'\u2500'
+    NEWLINE = u'\u23ce'
+
+
+def _format_newlines(prefix, formatted_node, options):
+    replacement = u''.join([
+        options.NEWLINE,
+        u'\n',
+        prefix])
+    return formatted_node.replace(u'\n', replacement)
+
+
+def _format_tree(node, options, prefix=u''):
+    children = list(node.children)
+    next_prefix = u''.join([prefix, options.VERTICAL, u'   '])
+    for child in children[:-1]:
+        yield u''.join([prefix,
+                        options.FORK,
+                        options.HORIZONTAL,
+                        options.HORIZONTAL,
+                        u' ',
+                        _format_newlines(next_prefix,
+                                         str(child),
+                                         options)])
+        for result in _format_tree(child,
+                                   options,
+                                   next_prefix):
+            yield result
+    if children:
+        last_prefix = u''.join([prefix, u'    '])
+        yield u''.join([prefix,
+                        options.LAST,
+                        options.HORIZONTAL,
+                        options.HORIZONTAL,
+                        u' ',
+                        _format_newlines(last_prefix,
+                                         str(children[-1]),
+                                         options)])
+        for result in _format_tree(children[-1],
+                                   options,
+                                   last_prefix):
+            yield result
+
+
+def print_tree(node):
+    lines = itertools.chain(
+        [str(node)],
+        _format_tree(node, Options()),
+        [u''],
+    )
+    print(u'\n'.join(lines))
--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-# import pygraphviz as pgv
-
-# A=pgv.AGraph()
-#
-# A.add_edge(1,2)
-# A.add_edge(2,3)
-# A.add_edge(1,3)
-#
-# print(A.string()) # print to screen
-# print("Wrote simple.dot")
-# A.write('simple.dot') # write to simple.dot
-#
-# B=pgv.AGraph('simple.dot') # create a new graph from file
-# B.layout() # layout with default (neato)
-# B.draw('simple.png') # draw png
-# print("Wrote simple.png")
 import DecisionTree
+import h5py
 
 tree = DecisionTree.Node(1)
-tree.add_child(3)
+tree.add_child(DecisionTree.Node(3))
+tree.add_child(DecisionTree.Node(4))
+tree.add_child(DecisionTree.Node(5))
+tree.children[1].add_child(DecisionTree.Node(5))
 
+DecisionTree.print_tree(tree)
+
+f = h5py.File('plant-classification.h5', 'r')
+dset = f["/data/int0"]
+
+D = []
+for col, l in enumerate(dset):
+    for row, value in enumerate(dset[col][0:10]):
+        if len(D) <= row:
+            D.append({"x": [], "y": []})
+        if col <= 5:
+            D[row]["x"].append(value)
+        else:
+            D[row]["y"].append(value)
+
+for row in D:
+    print(row)
